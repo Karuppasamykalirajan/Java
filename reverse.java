d16deafe82cb2896a4ef80d856423be9b9c0003e
Karuppasamykalirajan <k.karuppasamykalirajan@gmail.com>
2025-08-26 19:23:03 +0530

    Reversing a number

---
+++ reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String args[]){
+        int rev =0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number to reverse :");
+        int num = s.nextInt();
+        while(num!=0){
+            int n = num%10;
+            rev = rev*10+n;
+            num = num/10;
+        }
+        System.out.println(rev);
+
+    }
+}
