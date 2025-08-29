f4c1cc0b1c3ad2333bf5005cb6027755f2c0c7bf
Karuppasamykalirajan <k.karuppasamykalirajan@gmail.com>
2025-08-29 19:13:38 +0530

    pattern java

---
+++ pattern.java
@@ -0,0 +1,12 @@
+public class pattern {
+    public static void main(String[] args) {
+        int n =3;
+        for(int col =1;col<=n;col++){
+            for(int row=1;row<=n;row++){
+                System.out.print(row);
+                System.out.print("");
+            }
+            System.out.println();
+        }
+    }
+}
