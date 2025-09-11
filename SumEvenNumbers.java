925194ef14ab092457b994371473cccb6cac08af
Karuppasamy.k <161921820+Karuppasamykalirajan@users.noreply.github.com>
2025-09-11 23:31:54 +0530

    Sum of even numbers

---
+++ SumEvenNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumEvenNumbers {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int sum = 0;
+
+        for (int i = 2; i <= n; i += 2) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+    
+    }
+}
