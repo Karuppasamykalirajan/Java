dbd42d36d92415cb62c335c1df3a154f326fdaa8
Karuppasamy.k <161921820+Karuppasamykalirajan@users.noreply.github.com>
2025-09-11 23:38:08 +0530

    Harshad Number

---
+++ HarshadNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int sum = 0;
+        int temp = n;
+
+        
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (n % sum == 0) {
+            System.out.println("Harshad Number");
+        } else {
+            System.out.println("Not Harshad Number");
+        }
+    }
+}
