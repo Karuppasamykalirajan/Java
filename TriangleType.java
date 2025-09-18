3defb5b2a8a2d0c892067a116d05228dd9244d84
Karuppasamy.k <161921820+Karuppasamykalirajan@users.noreply.github.com>
2025-09-18 22:09:38 +0530

    Add files via upload

--- TriangleType.java
+++ TriangleType.java
@@ -2,27 +2,22 @@ import java.util.Scanner;
 
 public class TriangleType {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int side1 = s.nextInt();
-        int side2 = s.nextInt();
-        int side3 = s.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        if ((side1 + side2 > side3) && 
-            (side2 + side3 > side1) && 
-            (side1 + side3 > side2)) {
-
-            if (side1 == side2 && side2 == side3) {
-                System.out.println("Equilateral Triangle");
-            } else if (side1 == side2 || side2 == side3 || side1 == side3) {
-                System.out.println("Isosceles Triangle");
-            } else {
-                System.out.println("Scalene Triangle");
-            }
+    
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            System.out.println("Not a Valid Triangle");
+        } else if (a == b && b == c) {
+            System.out.println("Equilateral Triangle");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("Isosceles Triangle");
         } else {
-            System.out.println("Not a valid triangle");
+            System.out.println("Scalene Triangle");
         }
 
-        
     }
 }
