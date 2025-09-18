690f70cf1ab4823f2b0cebd2bfa53104b7749537
Karuppasamy.k <161921820+Karuppasamykalirajan@users.noreply.github.com>
2025-09-18 22:32:30 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,15 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        int n =3;
+        for(int col =1;col<=n;col++){
+            for(int row=1;row<=n;row++){
+                System.out.print(row);
+                System.out.print("");
+            }
+            System.out.println();
+        }
+    
+    }
+
+    
+}
---
+++ pattern4.java
@@ -0,0 +1,11 @@
+class pattern4 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'A'; ch <= 'C'; ch++) {
+                System.out.print(ch);
+            }
+            System.out.println(); 
+        }
+    }
+}
